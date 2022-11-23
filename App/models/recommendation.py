be16be89262d7c1f88dc75d59b673bad0ba03df3
Kristian-480 <krisrichards10@hotmail.com>
2022-11-23 23:32:52 +0000

    recommendation model (updated)

--- App/models/recommendation.py
+++ App/models/recommendation.py
@@ -1,22 +1,41 @@
 from App.database import db
-
 class Recommendation(db.Model):
-    recID = db.Column(db.Integer, primary_key=True)
-    sentFromStaffID = db.Column(db.Integer, db.ForeignKey('staff.staffID'))
-    sentToStudentID = db.Column(db.Integer, db.ForeignKey('student.studentID'))
-    recURL = db.Column(db.String, nullable=False)
+    recomID = db.Column(db.Integer, primary_key=True)
+    staffID = db.Column(db.Integer, db.ForeignKey('staff.staffID'))
+    studentID = db.Column(db.Integer, db.ForeignKey('student.studentID'))
+    staffName= db.Column(db.String, db.'staff.staffName'))
+    recomText= db.Column(db.String, nullable=False)
+    date= db.Column(db.String,nuallable=False)
+    recomList= db.relationship('recomList',backref=db.backref(Recommendation, lazy='joined'))
 
-    def __init__(self, sentFromStaffID,sentToStudentID, recURL):
-        self.sentFromStaffID = sentFromStaffID
-        self.sentToStudentID=sentToStudentID
-        self.recURL=recURL
+    def __init__(self,staffID, studentID,staffName, recomText,date ):
+        self.staffID=staffID
+        self.studentID= studentID
+        self.staffName=staffName
+        self.recomText= recomText
+        self.date=date
     
     def toJSON(self):
         return{
-            'recID': self.recID,
-            'sentFromStaffID': self.sentFromStaffID,
-            'sentToStudentID': self.sentToStudentID,
-            'recURL': self.recURL
+            'recomID'  : self.recomID,
+            'staffID'  : self.staffID,
+            'studentID': self.studentID,
+            'staffName': self.staffName,
+            'recomText': self.recomText,
+            'date'     : self.date
+            
+        }
+def toJSON_Recom(self):
+        return{
+            'recomID'  : self.recomID,
+            'staffID'  : self.staffID,
+            'studentID': self.studentID,
+            'staffName': self.staffName,
+            'recomText': self.recomText,
+            'date'     : self.date,
+            'recomList': [recom.toJSON() for recom in self.recomList]
+            
         }
 
+    
     
