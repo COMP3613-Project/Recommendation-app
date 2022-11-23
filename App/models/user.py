0ba1059395bce5709b77cb23535d78fa102d3b06
Kristian-480 <krisrichards10@hotmail.com>
2022-11-23 23:34:56 +0000

    user model (updated)

--- App/models/user.py
+++ App/models/user.py
@@ -2,27 +2,27 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    ID = db.Column(db.Integer, primary_key=True)
     email =  db.Column(db.String, unique=True,nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    userType = db.Column(db.String, nullable=False)
     firstName = db.Column(db.String, nullable=False)
     lastName = db.Column(db.String, nullable=False)
+    userType = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, email, password, userType, firstName, lastName):
+    def __init__(self, email, firstName, lastName,userType, password):
         self.email = email
-        self.set_password(password)
-        self.userType=userType
         self.firstName=firstName
         self.lastName=lastName
+        self.userType=userType
+        self.set_password(password)
 
     def toJSON(self):
         return{
             'id': self.id,
             'email': self.email,
-            'userType': self.userType,
             'firstName': self.firstName,
-            'lastName': self.lastName
+            'lastName': self.lastName,
+            'userType': self.userType
         }
 
     def set_password(self, password):
@@ -32,5 +32,4 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-
 
