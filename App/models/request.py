cd6b862f09cf26a8871be7fab71066d3a94b79c8
karishmaj20 <karishma.james@my.uwi.edu>
2022-11-27 21:15:30 +0000

    nameError fixed

--- App/models/request.py
+++ App/models/request.py
@@ -11,7 +11,7 @@ class Request(db.Model):
 	requestText= db.Column(db.String, nullable= False)
 	date= db.Column(db.String, nullable= False)
     
-	notification=db.relationship('Notification', backref= db.backref(staff,lazy='joined'))
+	notification=db.relationship('Notification', backref= db.backref('staff',lazy='joined'))
 	
 	def _init_(self,studentID, staffID,title,requestText,date):
 		self.studentID=studentID
