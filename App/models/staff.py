0c48e7a0696b732234cd4c7bd2e0ee8c04d5d7fa
karishmaj20 <karishma.james@my.uwi.edu>
2022-11-27 17:18:43 +0000

    correction to the referencing of the foreignKey

--- App/models/staff.py
+++ App/models/staff.py
@@ -3,7 +3,7 @@ from App.models import User
 
 
 class Staff(User):
-    staffID = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    staffID = db.Column(db.Integer, db.ForeignKey('user.ID'), primary_key=True)
     # staff has a list of notification objects
     notificationFeed = db.relationship('Notification', backref=db.backref('staff', lazy='joined'))
     requestList= db.relationship('Request', backref= db.backref('Request', lazy='joined'))
